f5f45f059c92b64b9d6671078d1268c2a19def18
Fai95 <38754252+Fai95@users.noreply.github.com>
2018-04-26 13:47:34 +0300

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+
+namespace CloudGamingApplication.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new CloudGamingApplication.App());
+
+            AppCenter.Start("fa00bbae-478a-4bff-a76f-591159be33ac", typeof(Analytics));
+        }
+    }
+}
